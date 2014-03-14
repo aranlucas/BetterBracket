4ad84515e8dfcb7c795bbc053dc9a7e591c43763
Justin Schoeff <jschoeff81690@gmail.com>
2014-03-14 16:04:05 -0400

    Added Bootstrap and a login function
    Check out how the login works and stuff.
    You can find it index.php/welcome/login

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -20,6 +20,10 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('welcome_message');
+	}	
+	public function login()
+	{
+		$this->load->view('auth/login');
 	}
 }
 
