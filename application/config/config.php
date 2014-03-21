5ef8dadb12d0abb3097361623d9398e4b4b13d38
Justin Schoeff <jschoeff81690@gmail.com>
2014-03-21 16:42:07 -0400

    fixed databse drivers stuffs

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '!<QLSK#K$KD(E*RH%Y^T&*(SL)Q!W@E#';
 
 /*
 |--------------------------------------------------------------------------
