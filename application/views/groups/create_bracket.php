90d6f0303c6f6584e414dacf6336db48d3544678
Justin Schoeff <jschoeff81690@gmail.com>
2014-04-08 14:05:08 -0400

    nothing

--- application/views/groups/create_bracket.php
+++ application/views/groups/create_bracket.php
@@ -14,13 +14,11 @@ $email = $this->session->userdata('email');
 <body>
 
 	<?php $this->load->view('template/navbar');?>
-	<div class="container">
-		<div class="jumbotron">
-			<h1>Create A Bracket</h1>
-		</div>
-	</div>
 
 	<div class="container-fluid">
+
+		<br> <br> <br>
+
 		<div class="row">
 			<div class="col-md-1">Round of 64</div>
 			<div class="col-md-1">Round of 32</div>
@@ -43,7 +41,8 @@ $email = $this->session->userdata('email');
 					2=>1,
 					3=>1,
 					4=>1,
-					5=>1
+					5=>1,
+					6=>1
 			);
 			$games = array(
 					array("fillergame for index 0")
@@ -55,16 +54,17 @@ $email = $this->session->userdata('email');
 					,array(3,14)
 					,array(7,10)
 					,array(2,15) );
+
+			$final_four_count = 1;
 			for($a = 1; $a< 4; $a+=2) {
-				$game_counts = array(8,4,2,1,1,0,0,0);
+				$game_counts = array(8,4,2,1,1,1,1,0);
 				for ($i=1; $i < 13; $i++) {
-					echo '<div id="round'.(($i<5)? $i : 12-$i +1).'" class="round col-md-1">';
+					$round = ($i<6)? $i : 12-$i +1;
+					$region = ($i<6)? $a : $a+1;
+					if($round == 7) $round = 6;
+					echo '<div id="round'.$round.'" class="round col-md-1">';
 					$count = ($i<6)?$game_counts[$i-1]+1:$game_counts[12-$i]+1;
 					for ($j=1; $j < $count; $j++) {
-
-						//calculate filler space for different round
-						$round = ($i<6)? $i : 12-$i +1;
-						$region = ($i<6)? $a : $a+1;
 						//round 2
 						if($round==2 ) {
 							echo '<div id="" class="filler-'.($j==1?"half":"game").'"></div>';
@@ -89,7 +89,7 @@ $email = $this->session->userdata('email');
 							echo '<div id="" class="filler-game"></div>';
 						}
 						//round 5
-						if($round==5 ) {
+						if($round== 5) {
 							if($a == 1) {
 								echo '<div id="" class="filler-half"></div>';
 								echo '<div id="" class="filler-game"></div>';
@@ -101,37 +101,57 @@ $email = $this->session->userdata('email');
 								echo '<div id="" class="filler-game"></div>';
 							}
 							echo '<div id="" class="split-game">';
-							echo '<a href="#"><div id="team1" class="team">';
-							echo '<p class="team-name"><em>'."".'</em>';
-							echo ' round'.$round.'game'.$counts[$round].'team</p>';
-							// echo ' round'.(($i<6)? $i : 12-$i +1).'game'.$j.'team';
-							echo '</div></a>';
+							$filler_id = $region.'-'.$round.'-'.$j.'-'.$counts[$round];
+
+                    		echo '<a href="#"><div id="'.( $round == 1 ? $identifier : $filler_id).'" class="team">';
+                    
+							echo '<strong class="seed">'.( $round == 1 ? $games[$counts[$round]][$c] : "").'</strong> ';
+		                    echo '<p class="team_name">'.($round == 1 ? $bracket[$region][$games[$counts[$round]][$c]]['team_name'] : "").'</p>';
+		                    echo '</div></a>';
 							echo '</div>';
 							$counts[$round]++;
+						}
+						if($round == 6) {
+							if($a != 1) {
+								echo '<div id="" class="split-game">';
+								$filler_id = '1-'.$round.'-'.$j.'-'.$counts[$round];
 
+	                    		echo '<a href="#"><div id="'.( $round == 1 ? $identifier : $filler_id).'" class="team">';
+	                    
+								echo '<strong class="seed">'.( $round == 1 ? $games[$counts[$round]][$c] : "").'</strong> ';
+			                    echo '<p class="team_name">'.( $round == 1 ? $bracket[$region][$games[$counts[$round]][$c]]['team_name'] : "").'</p>';
+			                    echo '</div></a>';
+								echo '</div>';
+							$counts[$round]++;
+							}
 
 						}
-						if($round != 5) {
-							echo '<div id="game'.$j.'" class="game">';
 
-							for ($c=0; $c < 2; $c++) {
-                	/*region-round-game-teamdid */
-                    $identifier = $region.'-'.$round.'-'.$j.'-'.$bracket[$region][$games[$counts[$round]][$c]]['team_id'];
-                    echo '<a href="#"><div id="'.$identifier.'" class="team">';
-                    //echo '<a href="#"><div id="team1" class="team">';
+						if($round < 5) {
+			                echo '<div id="game'.$j.'" class="game">';
 
-                    echo '<strong class="seed">'.( $round ==1 ? $games[$counts[$round]][$c] : "").'</strong> ';
-                    echo '<p class="team_name">'.($round == 1? $bracket[$region][$games[$counts[$round]][$c]]['team_name'] : "").'</p>';
-                    // echo ' round'.$round.'game'.$counts[$round].'</p>';
-                    // echo ' round'.(($i<6)? $i : 12-$i +1).'game'.$j.'team';
-                    echo '</div></a>';
-                }
-                echo '</div>';
-                $counts[$round]++;
-						}
-							
+			                for ($c=0; $c < 2; $c++) {
+			                	/*region-round-game-team_id */
+			                    $identifier = $region.'-'.$round.'-'.$j.'-'.$bracket[$region][$games[$counts[$round]][$c]]['team_id'];
+			                    /*region-round-game-team */
+			                    $filler_id = $region.'-'.$round.'-'.$j.'-'.($c+1);
+			                    
+
+			                    echo '<a href="#"><div id="'.( $round == 1 ? $identifier : $filler_id).'" class="team">';
+			                    //echo '<a href="#"><div id="team1" class="team">';
+
+			                    echo '<strong class="seed">'.( $round == 1 ? $games[$counts[$round]][$c] : "").'</strong> ';
+			                    echo '<p class="team_name">'.($round == 1? $bracket[$region][$games[$counts[$round]][$c]]['team_name'] : "").'</p>';
+			                    // echo ' round'.$round.'game'.$counts[$round].'</p>';
+			                    // echo ' round'.(($i<6)? $i : 12-$i +1).'game'.$j.'team';
+			                    echo '</div></a>';
+			                }
+			                echo '</div>';
+			                $counts[$round]++;
+			            }
+             
 					}
-
+					if($round < 6)
 					$counts[$round] =1;
 					echo '</div>';
 				}
@@ -145,6 +165,5 @@ $email = $this->session->userdata('email');
 
 	<?php $this->load->view('template/footer_scripts'); ?>
 	<script src="<?php echo base_url();?>assets/js/bracket.js"></script>
-
 </body>
 </html>
